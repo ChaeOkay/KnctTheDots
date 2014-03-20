a7e78a98942a81bb0604c90fc7896711aefad6cd
cokeefe <chaemobile@gmail.com>
2014-03-20 13:41:41 -0400

    Create Line model

---
+++ JCLine.h
@@ -0,0 +1,16 @@
+//
+//  JCLine.h
+//  KnctTheDots
+//
+//  Created by Chae O'Keefe on 3/20/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCLine : NSObject
+
+@property (nonatomic) CGPoint *startPt;
+@property (nonatomic) CGPoint *endPt;
+
+@end
---
+++ JCLine.m
@@ -0,0 +1,13 @@
+//
+//  JCLine.m
+//  KnctTheDots
+//
+//  Created by Chae O'Keefe on 3/20/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import "JCLine.h"
+
+@implementation JCLine
+
+@end
