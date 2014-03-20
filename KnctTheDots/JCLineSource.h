10555f8bef24d8f88a658f309b4b9833fffb4eb4
cokeefe <chaemobile@gmail.com>
2014-03-20 17:26:23 -0400

    Create LineSource protocol
    * This defines an interface for a line view to access the line
    * Using a protocol lowers our coupling to the JCLinesViewController type

---
+++ KnctTheDots/JCLineSource.h
@@ -0,0 +1,17 @@
+//
+//  JCLineSource.h
+//  KnctTheDots
+//
+//  Created by Chae O'Keefe on 3/20/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class JCLine;
+
+@protocol JCLineSource <NSObject>
+
+- (JCLine *) line;
+
+@end
